4f52572da66033a8130c8a963d24210e95fe48d9
Adam L. Taylor <adamlyletaylor@gmail.com>
2014-03-26 14:20:16 -0400

    Added comments and cleaned up apply_convert_units.m, used by Catalytic.  Should be no changes in function.

--- Applications/Catalytic/apply_convert_units.m
+++ Applications/Catalytic/apply_convert_units.m
@@ -1,12 +1,10 @@
-function [trx, didsomething] = apply_convert_units(trx,pxpermm,fps, alreadyconverted)
+function [trx, didsomething] = apply_convert_units(trx,pxpermm,fps,alreadyconverted)
 % split from convert_units_f 9/10/11 JAB
 
+% Determine whether pxpermm and/or fps have been provided as inputs.  If
+% so, use those values.  If not, use the values already in trx
 pxpermminput = exist('pxpermm','var');
 fpsinput = exist('fps','var');
-if ~exist( 'alreadyconverted', 'var' )
-   alreadyconverted = false;
-end
-
 if ~pxpermminput
   pxpermm = trx(1).pxpermm;
 end
@@ -14,16 +12,25 @@ if ~fpsinput
   fps = trx(1).fps;
 end
 
-%% actually do the conversion now
+% Determine whether the "main" field names (the fieldNamesThatKeepTheSameName below) have already
+% been converted from pels to mm
+if ~exist( 'alreadyconverted', 'var' )
+   alreadyconverted = false;
+end
 
-pxfns = {'xpred','ypred','dx','dy','v'};
+%
+%% actually do the conversion now
+%
+
+fieldNamesThatKeepTheSameName = {'xpred','ypred','dx','dy','v'};  % field names to be converted from pels to mm, keeping the same field name
 % these are used for plotting, so we want to keep them in pixels
-pxcpfns = {'x','y','a','b'};
+fieldNamesThatGetMMAppended = {'x','y','a','b'};  % field names to be converted from pels to mm, but the new field has "_mm" appended to it (and the original field is kept)
 okfns = {'x','y','theta','a','b','id','moviename','firstframe','arena',...
   'f2i','nframes','endframe','xpred','ypred','thetapred','dx','dy','v',...
-  'a_mm','b_mm','x_mm','y_mm','matname','sex','type','timestamps'};
-unknownfns = setdiff(getperframepropnames(trx),okfns);
+  'a_mm','b_mm','x_mm','y_mm','matname','sex','type','timestamps'};  % list of field names we know how to deal with? (ALT, 2014-03-26)
+unknownfns = setdiff(getperframepropnames(trx),okfns);  % if this is non-empty, could be problematic
 
+% If needed, convert the fields that keep the same field name
 if ~alreadyconverted
    if ~isempty(unknownfns),
      b = questdlg({'Do not know how to convert the following variables: ',...
@@ -34,47 +41,62 @@ if ~alreadyconverted
      end
    end
 
-   for ii = 1:length(pxfns),
-     fn = pxfns{ii};
+   for i = 1:length(fieldNamesThatKeepTheSameName),
+     fn = fieldNamesThatKeepTheSameName{i};
      if isfield(trx,fn),
-       for fly = 1:length(trx),
-         trx(fly).(fn) = trx(fly).(fn) / pxpermm;
+       for iFly = 1:length(trx),
+         trx(iFly).(fn) = trx(iFly).(fn) / pxpermm;
        end
      end
    end
 end
 
+% Convert the fields that get "_mm" appended to the field name, keeping the
+% original field intact
 didsomething = false;
-for ii = 1:length(pxcpfns),
-  fn = pxcpfns{ii};
+for i = 1:length(fieldNamesThatGetMMAppended),
+  fn = fieldNamesThatGetMMAppended{i};
   newfn = [fn,'_mm'];
   if isfield(trx,fn),
-    for fly = 1:length(trx),
-      trx(fly).(newfn) = trx(fly).(fn) / pxpermm;
+    for iFly = 1:length(trx),
+      trx(iFly).(newfn) = trx(iFly).(fn) / pxpermm;
       didsomething = true;
     end
   end
 end
 
-for fly = 1:length(trx),
+% If the user has supplied pxpermm or fpsinput, the output trx file should
+% have those values in it
+for iFly = 1:length(trx),
   if pxpermminput && ~alreadyconverted,
-    trx(fly).pxpermm = pxpermm;
+    trx(iFly).pxpermm = pxpermm;
   end
-  if fpsinput,
-     if ~alreadyconverted
-        trx(fly).fps = fps;
-     end
+  if fpsinput && ~alreadyconverted ,
+      trx(iFly).fps = fps;
   end
 end
-  
+
+% If there are no timestamps, synthesize some
 if ~isfield( trx, 'timestamps' )
    if isfield( trx, 'fps' )
       fprintf( 1, 'no timestamps saved in file -- faking\n' );
-      for fly = 1:length(trx),
-        trx(fly).timestamps = (trx(fly).firstframe:trx(fly).endframe)/fps;
+      for iFly = 1:length(trx),
+        trx(iFly).timestamps = (trx(iFly).firstframe:trx(iFly).endframe)/fps;
       end
       didsomething = true;
    else
       fprintf( 1, 'no timestamps saved in file and no fps number to calculate from\n' );
    end
 end
+
+% % If theta is a field name, "convert" to mm, because JAABA seems to need a
+% % theta_mm field.  Of course, since theta is in radians to start with,
+% % theta_mm is just a copy of theta.
+% if isfield(trx,'theta') ,
+%   for iFly = 1:length(trx) ,
+%     trx(iFly).theta_mm=trx(iFly).theta;
+%   end
+%   didsomething = true;
+% end
+
+end  % function
